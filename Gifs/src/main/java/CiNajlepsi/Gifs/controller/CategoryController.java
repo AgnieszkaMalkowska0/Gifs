13a90db8df9753a7a54bf450cb0ee7fc2c16b9a2
Agnieszka Malkowska <amalkowska@yahoo.com>
2019-07-23 14:27:51 +0200

    Categories

--- Gifs/src/main/java/CiNajlepsi/Gifs/controller/CategoryController.java
+++ Gifs/src/main/java/CiNajlepsi/Gifs/controller/CategoryController.java
@@ -1,5 +1,6 @@
 package CiNajlepsi.Gifs.controller;
 
+import CiNajlepsi.Gifs.model.Category;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,14 +8,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 /**
  * Created by Agnieszka on 23.07.2019.
  */
-//Karol Commit bla bla bla
 @Controller
-public class GifController {
+public class CategoryController {
 
-    @GetMapping("/")
-    public String home(ModelMap map) {
-        map.put("gifs",Gif.get)
-        return "home";
+    @GetMapping("/categories")
+    public String index(ModelMap map) {
+        map.put("categories", Category.getCategories());
+        return  "categories";
     }
 
 
