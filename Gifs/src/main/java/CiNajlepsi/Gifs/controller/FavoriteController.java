9c9cbfdc3e77b0cfbec6591a1a11b8055eb1930f
Karol Janaszek <kar.jan94@gmail.com>
2019-07-23 15:06:23 +0200

    addef possibility to enter favorite and faorite java data

---
+++ Gifs/src/main/java/CiNajlepsi/Gifs/controller/FavoriteController.java
@@ -0,0 +1,14 @@
+package CiNajlepsi.Gifs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FavoriteController {
+    @GetMapping("/favorites")
+    public String index(ModelMap map) {
+        map.put("favorites", Gif.getFavorites());
+        return "favorites";
+    }
+}
--- Gifs/src/main/java/CiNajlepsi/Gifs/controller/Gif.java
+++ Gifs/src/main/java/CiNajlepsi/Gifs/controller/Gif.java
@@ -2,26 +2,47 @@ package CiNajlepsi.Gifs.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Gif {
     private String name;
+
     public Gif(String name) {
         this.name = name;
     }
-    public String getUrl(){
+
+    public String getUrl() {
         return "gifs/" + name + ".gif";
     }
+
     public void setName(String name) {
         this.name = name;
     }
-    public static List<Gif> getGifs(){
+
+    public static List<Gif> getGifs() {
         List<Gif> gifs = new ArrayList<>();
         gifs.add(new Gif("android-explosion"));
-        gifs.add(new Gif("ben-and-mike"));
-        gifs.add(new Gif("book-dominos"));
+        gifs.add(new Gif("ben-and-mike", true));
+        gifs.add(new Gif("book-dominos", true));
         gifs.add(new Gif("compiler-bot"));
         gifs.add(new Gif("cowboy-coder"));
-        gifs.add(new Gif("infinite-andrew"));
+        gifs.add(new Gif("infinite-andrew", true));
         return gifs;
     }
+
+    public static List<Gif> getFavorites() {
+        return getGifs().stream().filter(gif -> gif.favorite).collect(Collectors.toList());
+    }
+
+    private boolean favorite;
+
+    public Gif(String name, boolean favorite) {
+        this.name = name;
+        this.favorite = favorite;
+    }
+
+    public boolean getFavorite() {
+        return favorite;
+    }
 }
+
